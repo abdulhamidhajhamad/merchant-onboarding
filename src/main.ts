0ac77203b6eb41be9f49f29e34c7adc8eba1f4fa
abdulhamidhajhamad <abdulhamidhajhamad@gmail.com>
2026-09-09 20:52:28 +0300

    adding swagger feat

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Merchant Onboarding API')
+    .setDescription('Production-grade merchant onboarding service')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
